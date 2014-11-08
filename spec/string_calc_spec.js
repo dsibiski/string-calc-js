597b624c77d7a6ff6e158e913078ae4d5faa49e2
Dave Sibiski <dsibiski@gmail.com>
2014-11-07 23:46:46 -0400

    Handles multiple numbers seperated by a comma

--- spec/string_calc_spec.js
+++ spec/string_calc_spec.js
@@ -23,6 +23,23 @@ describe('StringCalc', function() {
       });
     });
 
+    describe('given 2 numbers', function () {
+      it('returns the sum', function () {
+        var sut = new StringCalc('1,2');
+        expect(sut.add()).toEqual(3);
+      });
+
+      it('returns 7 given "3,4"', function () {
+        var sut = new StringCalc('3,4');
+        expect(sut.add()).toEqual(7);
+      });
+
+      it('returns 46 given "12,34"', function () {
+        var sut = new StringCalc('12,34');
+        expect(sut.add()).toEqual(46);
+      });
+    });
+
   });
 
 });
