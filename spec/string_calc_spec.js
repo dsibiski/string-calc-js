a3db64493d62cd2e0d9acc50cd416e235c272169
Dave Sibiski <dsibiski@gmail.com>
2014-11-10 17:14:32 -0400

    Converts to 4 spaces for a tab

--- spec/string_calc_spec.js
+++ spec/string_calc_spec.js
@@ -2,57 +2,57 @@ var StringCalc = require('../lib/string_calc');
 
 describe('StringCalc', function() {
 
-  describe('.add', function () {
+    describe('.add', function () {
 
-    describe('given an empty string', function() {
-      it('returns 0', function() {
-        var sut = StringCalc('');
-        expect(sut.add()).toEqual(0);
-      });
+        describe('given an empty string', function() {
+            it('returns 0', function() {
+                var sut = StringCalc('');
+                expect(sut.add()).toEqual(0);
+            });
+        });
+
+        describe('given a single number', function() {
+            it('returns 1', function () {
+                var sut = StringCalc('1');
+                expect(sut.add()).toEqual(1);
+            });
+
+            it('returns 2 given "2"', function () {
+                var sut = StringCalc('2');
+                expect(sut.add()).toEqual(2);
+            });
+        });
+
+        describe('given 2 numbers', function () {
+            it('returns the sum', function () {
+                var sut = StringCalc('1,2');
+                expect(sut.add()).toEqual(3);
+            });
+
+            it('returns 7 given "3,4"', function () {
+                var sut = StringCalc('3,4');
+                expect(sut.add()).toEqual(7);
+            });
+
+            it('returns 46 given "12,34"', function () {
+                var sut = StringCalc('12,34');
+                expect(sut.add()).toEqual(46);
+            });
+        });
+
+        describe('given 3 numbers', function () {
+            it('returns the sum', function () {
+                var sut = StringCalc('1,2,3');
+                expect(sut.add()).toEqual(6);
+            });
+
+            it('returns 66 for "11,22,33"', function () {
+                var sut = StringCalc('11,22,33');
+                expect(sut.add()).toEqual(66);
+            });
+        });
+
     });
-
-    describe('given a single number', function() {
-      it('returns 1', function () {
-        var sut = StringCalc('1');
-        expect(sut.add()).toEqual(1);
-      });
-
-      it('returns 2 given "2"', function () {
-        var sut = StringCalc('2');
-        expect(sut.add()).toEqual(2);
-      });
-    });
-
-    describe('given 2 numbers', function () {
-      it('returns the sum', function () {
-        var sut = StringCalc('1,2');
-        expect(sut.add()).toEqual(3);
-      });
-
-      it('returns 7 given "3,4"', function () {
-        var sut = StringCalc('3,4');
-        expect(sut.add()).toEqual(7);
-      });
-
-      it('returns 46 given "12,34"', function () {
-        var sut = StringCalc('12,34');
-        expect(sut.add()).toEqual(46);
-      });
-    });
-
-    describe('given 3 numbers', function () {
-      it('returns the sum', function () {
-        var sut = StringCalc('1,2,3');
-        expect(sut.add()).toEqual(6);
-      });
-
-      it('returns 66 for "11,22,33"', function () {
-        var sut = StringCalc('11,22,33');
-        expect(sut.add()).toEqual(66);
-      });
-    });
-
-  });
 
 });
 
